ed326d6e67d26a0407420146823ced5499eeeb76
Ummar Hamid <kaxama34@gmail.com>
2022-07-24 10:16:15 +0530

    Finalize theming

--- web/themes/custom/drupaltaskspecbee/dist/components/molecules/slick-slider/slick-slider.js
+++ web/themes/custom/drupaltaskspecbee/dist/components/molecules/slick-slider/slick-slider.js
@@ -1,23 +1,31 @@
 "use strict";
 (($) => {
     $(function ($) {
-        $('.slider-container-custom').slick({
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            arrows: true,
-            mobileFirst: false,
-            responsive: [
-                {
-                    breakpoint: 768,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                        dots: true,
-                        arrows: false,
+        function slickCarousal() {
+            $('.slider-container-custom').slick({
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                arrows: true,
+                mobileFirst: false,
+                responsive: [
+                    {
+                        breakpoint: 768,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                            dots: true,
+                            arrows: false,
+                        },
                     },
-                },
-            ],
+                ],
+            });
+        }
+        slickCarousal();
+        $('.quicktabs-loaded').click(function () {
+            $('.slider-container-custom').slick('unslick');
+            slickCarousal();
+            $('.slider-container-custom').hide().fadeIn();
         });
     });
 })(jQuery);
